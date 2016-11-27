0e0082a00249b7db65e9e59420be4d2a3cc031ad
watanabe_shuichi <kh01170523@gmail.com>
2016-11-27 08:56:55 +0000

    編集機能・一覧機能完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,7 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+    @new_posts = Post.all
   end
 
   def show
@@ -17,9 +19,13 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to "/posts/#{@post.id}"
   end
 
   def destroy
